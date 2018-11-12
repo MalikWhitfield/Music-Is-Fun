e25fd2fc89e86b73fd6c921133d2f87e327b8949
Malik Whitfield <malikwhitfield29@gmail.com>
2018-11-12 13:17:41 -0700

    github push

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -1,4 +1,5 @@
 import ItunesService from "./itunes-service.js";
+import Song from "../../models/Song.js";
 
 //PRIVATE
 
@@ -7,8 +8,28 @@ const itunesService = new ItunesService()
 function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
-
-
+  let template = ''
+  for (let i = 0; i < results.length; i++) {
+    let song = results[i]
+    let newCollection = song.collection
+    template += `
+    <div class="pb-3 pl-2 pr-2">
+    <div class="card cardStyle justify-content-center" style="width: 18rem;">
+            <img class="card-img-top" src="${results[i].albumArt}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-text"> ${results[i].title.substring(0, 20)}
+                  </h5>
+                    <p class="card-text"> ${results[i].artist.substring(0, 20)} </p>
+                    <p class="card-text">${!newCollection ? '' : results[i].collection.substring(0, 20)} </p >
+      <p class="card-text"> ${results[i].price} </p>
+      <audio controls style="width: 100%;">
+        <source src="${results[i].preview}" type="audio/mpeg">
+                    </audio>
+                </div>
+                </div >
+        </div > `
+  }
+  document.getElementById('songs').innerHTML = template
 
 }
 
